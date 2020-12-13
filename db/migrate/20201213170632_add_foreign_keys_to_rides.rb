39765aaa06a3d67af95168c39c09550438d706a2
giegeralexandra <giegeralexandra@gmail.com>
2020-12-13 12:09:50 -0500

    Done.

---
+++ db/migrate/20201213170632_add_foreign_keys_to_rides.rb
@@ -0,0 +1,8 @@
+class AddForeignKeysToRides < ActiveRecord::Migration[5.0]
+  def change
+    change_table :rides do |t|
+      t.belongs_to :passenger
+      t.belongs_to :taxi
+    end
+  end
+end
